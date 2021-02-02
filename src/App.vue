a4aebda5133bbaba4c721a7578d24031eb078051
dtaib <dtaib@wiremuch.com>
2021-02-02 23:08:18 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="flex">
+      <img class="block mx-auto" alt="Vue logo" src="./assets/logo.png" />
+    </div>
+    <HelloWorld msg="Welcome to Your Vue 3 + TypeScript + TailwindCSS BoilerPlate" />
+  </div>
+</template>
+
+<script lang="ts">
+import "tailwindcss/tailwind.css";
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
